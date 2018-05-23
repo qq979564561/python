be329836ac91a4aa87f970e46250f6aae0f68aff
Toky <15988388619@163.com>
2018-05-24 01:06:02 +0800

    5.24 before dawn

--- lxf/regular_expression.py
+++ lxf/regular_expression.py
@@ -7,3 +7,10 @@ else:
     print('no')
 '''
 print(re.split(r'\s+','a b    c'))
+print(re.split(r'[\s\,\;]+','a,b,  c;1,2,   3'))
+print(re.split(r'[\s\,\;]+', 'a,b;; c  d'))
+m = re.match(r'^(\d{3})-(\d{3,8})$', '010-12345')
+print(m)
+print(m.group(0)) #Original string
+print(m.group(1))
+print(m.group(2))
